4a6b8cbf4dbf3f23dbe19be0e62d8680b46a7224
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2023-05-24 22:49:58 +0200

    Changes

---
+++ OperatorokFeladat/Kifejezes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorokFeladat
+{
+    internal class Kifejezes
+    {
+        int elsoOperandus;
+        string operatorValtozo;
+        int masodikOperandus;
+
+        public Kifejezes(int elsoOperandus, string operatorValtozo, int masodikOperandus)
+        {
+            this.elsoOperandus = elsoOperandus;
+            this.operatorValtozo = operatorValtozo;
+            this.masodikOperandus = masodikOperandus;
+        }
+
+        public int ElsoOperandus { get => elsoOperandus; }
+        public string OperatorValtozo { get => operatorValtozo; }
+        public int MasodikOperandus { get => masodikOperandus; }
+
+        public Kifejezes(string szovegSor)
+        {
+            this.elsoOperandus = Convert.ToInt32(szovegSor.Split()[0]);
+            this.operatorValtozo = szovegSor.Split()[1];
+            this.masodikOperandus = Convert.ToInt32(szovegSor.Split()[2]);
+        }
+    }
+}
--- OperatorokFeladat/Program.cs
+++ OperatorokFeladat/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace OperatorokFeladat
+﻿using System.Text;
+
+namespace OperatorokFeladat
 {
     internal class Program
     {
+        public static List<Kifejezes> lista = new List<Kifejezes>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Beolvasas("kifejezesek.txt");
+            AllomanyHanyKifejezes(lista);
+            MaradekosOsztasOperandusai(lista);
+            VanETizzelOszthato(lista);
+        }
+
+        private static void VanETizzelOszthato(List<Kifejezes> lista)
+        {
+            Console.WriteLine("4. feladat:");
+            if (lista.Any(x => x.ElsoOperandus % 10 == 0 && x.MasodikOperandus % 10 == 0))
+            {
+                Console.WriteLine("Van!");
+            }
+            else
+            {
+                Console.WriteLine("Nincs!");
+            };
+        }
+
+        private static void MaradekosOsztasOperandusai(List<Kifejezes> lista)
+        {
+            Console.WriteLine("3.feladat:");
+            lista.ForEach(x =>
+            {
+                if (x.OperatorValtozo == "mod")
+                {
+                    Console.WriteLine($"{x.ElsoOperandus};{x.MasodikOperandus}");
+                }
+            });
+        }
+
+        private static void AllomanyHanyKifejezes(List<Kifejezes> lista)
+        {
+            Console.WriteLine($"2. feladat:\n Az állomány {lista.Count} db kifejezést tartlmaz");
+        }
+
+        private static void Beolvasas(string eleresiUt)
+        {
+            File.ReadAllLines(eleresiUt, Encoding.UTF8).ToList().ForEach(x => { lista.Add(new Kifejezes(x)); });
         }
     }
 }
