1fae29f532e92c012b5f19c2e37bca8455751f7b
gazdi435 <72067999+gazdi435@users.noreply.github.com>
2023-05-24 21:46:03 +0200

    Add project files.

---
+++ OperatorokFeladat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OperatorokFeladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
